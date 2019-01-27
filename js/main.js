c5cf0da638c2c0bf3e3d2085064860480c21d58a
aces8492 <aces8492@gmail.com>
2019-01-28 05:12:18 +0900

    milkey-way

--- js/main.js
+++ js/main.js
@@ -1,2 +1,16 @@
+function multiply(num1, num2) {
+	var result = num1 * num2;
+	return result;
+}
+
 var myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello world!';
+myHeading.textContent = 'Show 10*10 result when click this!';
+
+var mult_result = 0;
+mult_result = multiply(10,10); 
+
+document.querySelector('html').onClick = function() {
+	alert('ok');
+}
+
+
